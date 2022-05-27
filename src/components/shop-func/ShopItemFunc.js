0caa435c4b8f5a431e674b0a4ff265ce2c687662
o-I-V-A-N-o <v691080@mail.ru>
2022-05-27 21:35:08 +0700

    First commit

---
+++ src/components/shop-func/ShopItemFunc.js
@@ -0,0 +1,26 @@
+//import './func.css';
+
+function ShopItemFunc(props) {
+        
+    return (
+        <div className="main-content">
+            <h3>ShopItemFunc</h3><br/>
+            <h2>{props.item.brand}</h2>
+            <h1>{props.item.title}</h1>
+            <h3>{props.item.description}</h3>
+            <div className="description">
+                {props.item.descriptionFull}
+            </div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+            <div className="price">{props.item.currency}{props.item.price.toFixed(2)}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
+
+export default ShopItemFunc;
